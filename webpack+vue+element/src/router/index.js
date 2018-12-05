2a4987bfb0f3d606a3e4221bfa5314f01473f0b5
usecodelee <usecode@163.com>
2018-12-05 16:50:48 +0800

    修改文件名报错调整

--- webpack+vue+element/src/router/index.js
+++ webpack+vue+element/src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import html2img from '@/components/html2img'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [{
+        path: '/',
+        name: 'html2img',
+        component: html2img
+    }]
 })
